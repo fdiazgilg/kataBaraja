5ee42da0605a1ad249a2793efb8ec8a06250ddb6
fdiazgilg <fdiazgilg@gmail.com>
2019-11-25 23:47:39 +0100

    Versión funcional de baraja

---
+++ baraja.py
@@ -0,0 +1,23 @@
+import random
+_palos = ['o','c','e','b']
+_cartas = ['A','2','3','4','5','6','7','S','C','R']
+
+def baraja():
+    result = []
+    for palo in _palos:
+        for carta in _cartas:
+            result.append(carta+palo)
+
+    return result
+
+def elige_carta(i, longitud):
+    return random.randint(0,longitud-1)
+
+
+def mezclar(b):
+    for i in range(len(b)):
+        al_azar = elige_carta(i, len(b))
+        aux = b[i]
+        b[i] = b[al_azar]
+        b[al_azar] = aux
+    return b
