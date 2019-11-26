00bae24d04c6a37d0b3e41509f169d9899532acc
fdiazgilg <fdiazgilg@gmail.com>
2019-11-26 01:21:10 +0100

    Version Repartir

--- baraja.py
+++ baraja.py
@@ -21,3 +21,39 @@ def mezclar(b):
         b[i] = b[al_azar]
         b[al_azar] = aux
     return b
+
+class Baraja():
+    naipes = []
+    __palos = ['o','c','e','b']
+    __cartas = ['A','2','3','4','5','6','7','S','C','R']
+
+    def __init__(self):
+        self.naipes = []
+        for palo in _palos:
+            for carta in _cartas:
+                self.naipes.append(carta+palo)
+    
+    def elige_carta(self, i):
+        return random.randint(0,len(self.naipes)-1)
+
+
+    def mezclar(self):
+        for i in range(len(self.naipes)):
+            al_azar = self.elige_carta(i, len(self.naipes))
+            aux = self.naipes[i]
+            self.naipes[i] = self.naipes[al_azar]
+            self.naipes[al_azar] = aux
+
+    def repartir(self, mano, jugadores):
+        jugadas = []
+        for i in range(jugadores):
+            jugadas.append([])
+            for j in range(mano):
+                jugadas[i].append(self.naipes[j*2+i])
+        return jugadas
+    
+            
+
+
+
+
